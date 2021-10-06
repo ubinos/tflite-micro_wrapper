29e902ce6cbeca5e58b24e92ea70968081684f3b
Sungho Park <siminsungho@gmail.com>
2021-10-06 09:11:55 +0900

    refactoring

--- config/tflite_micro_nucleof207zg_baremetal.cmake
+++ config/tflite_micro_nucleof207zg_baremetal.cmake
@@ -9,8 +9,9 @@
 set_cache(PROJECT_TOOLCHAIN_C_STD "GNU11" STRING)
 set_cache(PROJECT_TOOLCHAIN_CXX_STD "GNU++14" STRING)
 set_cache(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT FALSE BOOL)
+set_cache(UBINOS__BSP__CMSIS_INCLUDE_DIR "${PROJECT_LIBRARY_DIR}/CMSIS_5/CMSIS/Core/Include" PATH)
 
-include(${PROJECT_LIBRARY_DIR}/CMSIS_5_wrapper/config/cmsis_5.cmake)
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nucleof207zg_baremetal.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/tflite_micro.cmake)
+include(${PROJECT_LIBRARY_DIR}/CMSIS_5_wrapper/config/cmsis_5.cmake)
+include(${PROJECT_LIBRARY_DIR}/tflite-micro_wrapper/config/tflite_micro.cmake)
 
