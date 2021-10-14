092d1117c1e1553e3b7e0c063c62834d91f1d0ba
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:39:18 +0900

    update ubinos build scripts

--- config/tflite_micro.cmake
+++ config/tflite_micro.cmake
@@ -1,4 +1,5 @@
 set(INCLUDE__TFLITE_MICRO TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} tflite-micro_wrapper)
 
 set_cache_default(TFLITE_MICRO__BASE_DIR "${PROJECT_LIBRARY_DIR}/tflite-micro" STRING "tflite-micro project base dir")
 
