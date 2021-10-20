345d7b153880bfa034ffe0875fb48ddf7e94d785
Sungho Park <siminsungho@gmail.com>
2021-10-20 20:36:28 +0900

    add clone function to ubinos config selecter

--- config/tflite_micro.cmake
+++ config/tflite_micro.cmake
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2021 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 set(INCLUDE__TFLITE_MICRO TRUE)
 set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} tflite-micro_wrapper)
 
--- config/tflite_micro_nucleof207zg_baremetal.cmake
+++ config/tflite_micro_nucleof207zg_baremetal.cmake
@@ -1,10 +1,10 @@
 #
-# Copyright (c) 2019 Sung Ho Park and CSOS
+# Copyright (c) 2021 Sung Ho Park and CSOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# {ubinos_config_type: [buildable, cmake, lib]}
+# ubinos_config_info {"name_base": "tflite_micro", "build_type": "cmake_ubinos"}
 
 set_cache(PROJECT_TOOLCHAIN_C_STD "GNU11" STRING)
 set_cache(PROJECT_TOOLCHAIN_CXX_STD "GNU++14" STRING)
@@ -13,5 +13,6 @@ set_cache(UBINOS__BSP__CMSIS_INCLUDE_DIR "${PROJECT_LIBRARY_DIR}/CMSIS_5/CMSIS/C
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nucleof207zg_baremetal.cmake)
 include(${PROJECT_LIBRARY_DIR}/CMSIS_5_wrapper/config/cmsis_5.cmake)
-include(${PROJECT_LIBRARY_DIR}/tflite-micro_wrapper/config/tflite_micro.cmake)
+
+include(${CMAKE_CURRENT_LIST_DIR}/tflite_micro.cmake)
 
