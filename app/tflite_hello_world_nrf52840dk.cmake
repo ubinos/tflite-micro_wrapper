f27c174e449c2dc0d16b75aed0c37a96aec988b0
Sungho Park <siminsungho@gmail.com>
2022-01-17 16:48:49 +0900

    add ubinos config tflite_hello_world_nrf52840dk

---
+++ app/tflite_hello_world_nrf52840dk.cmake
@@ -0,0 +1,56 @@
+#
+# Copyright (c) 2022 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "tflite_hello_world", "build_type": "cmake_ubinos", "app": true}
+
+set_cache(PROJECT_TOOLCHAIN_C_STD "GNU11" STRING)
+set_cache(PROJECT_TOOLCHAIN_CXX_STD "GNU++14" STRING)
+set_cache(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT FALSE BOOL)
+
+set_cache(UBINOS__BSP__CMSIS_INCLUDE_DIR "${PROJECT_LIBRARY_DIR}/CMSIS_5/CMSIS/Core/Include" PATH)
+
+set_cache(UBINOS__UBIK__TICK_TYPE "RTC" STRING)
+set_cache(UBINOS__UBIK__TICK_PER_SEC 1024 STRING)
+
+set_cache(UBINOS__UBICLIB__EXCLUDE_CLI FALSE BOOL)
+
+set_cache(NRF5SDK__BSP_DEFINES_ONLY TRUE BOOL)
+set_cache(NRF5SDK__NRFX_POWER_ENABLED FALSE BOOL)
+
+set_cache(UBINOS__BSP__DTTY_TYPE "EXTERNAL" STRING)
+set_cache(NRF5SDK__DTTY_NRF_UART_ENABLE TRUE BOOL)
+set_cache(NRF5SDK__UART_ENABLED TRUE BOOL)
+set_cache(NRF5SDK__NRFX_UARTE0_ENABLED TRUE BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52840dk.cmake)
+include(${PROJECT_LIBRARY_DIR}/CMSIS_5_wrapper/config/cmsis_5.cmake)
+include(${PROJECT_LIBRARY_DIR}/seggerrtt_wrapper/config/seggerrtt.cmake)
+include(${PROJECT_LIBRARY_DIR}/nrf5sdk_wrapper/config/nrf5sdk.cmake)
+include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
+include(${PROJECT_LIBRARY_DIR}/tflite-micro_wrapper/config/tflite_micro.cmake)
+
+####
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "tflite_hello_world")
+
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+string(TOLOWER ${UBINOS__BSP__BOARD_MODEL} _temp_board_model)
+set(_temp_softdevice_name "blank")
+
+include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model}/${_temp_softdevice_name}/config)
+include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model})
+include_directories(${_tmp_source_dir})
+
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.cc"
+    "${_tmp_source_dir}/*.S"
+    "${_tmp_source_dir}/*.s")
+
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
+
--- make/batch.mk
+++ make/batch.mk
@@ -23,6 +23,13 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=tflite_hello_world_nrf52840dk
+	@echo ""
+	@echo ""
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=tflite_hello_world_nucleof207zg
 	@echo ""
 	@echo ""
