64ec6e9df5c1fee628e998472b0a7837d84bb40f
Sungho Park <siminsungho@gmail.com>
2023-01-20 16:45:11 +0900

    refactoring.

--- app/tflite_hello_world_local_llvm_baremetal.cmake
+++ app/tflite_hello_world_local_llvm_baremetal.cmake
@@ -21,11 +21,7 @@ set(INCLUDE__APP TRUE)
 set(APP__NAME "tflite_hello_world")
 
 get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
-string(TOLOWER ${UBINOS__BSP__BOARD_MODEL} _temp_board_model)
-set(_temp_softdevice_name "blank")
 
-include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model}/${_temp_softdevice_name}/config)
-include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model})
 include_directories(${_tmp_source_dir})
 
 file(GLOB_RECURSE _tmp_sources
