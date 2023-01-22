25ff41d792b935bac3dd01182db85c1617f5efd6
Sungho Park <siminsungho@gmail.com>
2023-01-23 08:41:39 +0900

    add ubinos config, tflite_micro_memory_helpers_test_local_llvm_baremetal, tflite_micro_micro_allocation_info_test_local_llvm_baremetal, tflite_micro_micro_allocator_test_local_llvm_baremetal, tflite_micro_micro_context_test_local_llvm_baremetal, tflite_micro_micro_interpreter_test_local_llvm_baremetal, tflite_micro_micro_log_test_local_llvm_baremetal, tflite_micro_micro_mutable_op_resolver_test_local_llvm_baremetal, tflite_micro_micro_resource_variable_test_local_llvm_baremetal, tflite_micro_micro_string_test_local_llvm_baremetal, tflite_micro_micro_time_test_local_llvm_baremetal, tflite_micro_micro_utils_test_local_llvm_baremetal, tflite_micro_recording_micro_allocator_test_local_llvm_baremetal, tflite_micro_testing_helpers_test_local_llvm_baremetal.

---
+++ app/tflite_micro_micro_allocation_info_test_local_llvm_baremetal.cmake
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2023 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "tflite_micro_micro_allocation_info_test", "build_type": "cmake_ubinos", "app": true}
+
+set_cache(PROJECT_TOOLCHAIN_C_STD "GNU11" STRING)
+set_cache(PROJECT_TOOLCHAIN_CXX_STD "GNU++14" STRING)
+set_cache(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT FALSE BOOL)
+
+set_cache(TFLITE_MICRO__INCLUDE_CMSIS_NN FALSE BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_local_llvm_baremetal.cmake)
+include(${PROJECT_LIBRARY_DIR}/tflite-micro_wrapper/config/tflite_micro.cmake)
+include(${PROJECT_LIBRARY_DIR}/googletest_wrapper/config/googletest.cmake)
+
+####
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "tflite_micro_micro_allocation_info_test")
+
+get_filename_component(_tmp_source_dir "${TFLITE_MICRO__BASE_DIR}/tensorflow/lite/micro" ABSOLUTE)
+
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_source_dir}/micro_allocation_info_test.cc)
+
+add_definitions("-DTF_LITE_STATIC_MEMORY")
+add_definitions("-DTF_LITE_MCU_DEBUG_LOG")
