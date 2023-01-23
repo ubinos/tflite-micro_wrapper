f797fd36ebe41f949efa7fc3c928f345181320eb
Sungho Park <siminsungho@gmail.com>
2023-01-24 01:44:20 +0900

    refactoring.

--- app/tflite_hello_world_nrf52840dk_baremetal.cmake
+++ app/tflite_hello_world_nrf52840dk_baremetal.cmake
@@ -17,9 +17,6 @@ set_cache(NRF5SDK__NRFX_POWER_ENABLED FALSE BOOL)
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52840dk_baremetal.cmake)
 include(${PROJECT_LIBRARY_DIR}/CMSIS_5_wrapper/config/cmsis_5.cmake)
-include(${PROJECT_LIBRARY_DIR}/seggerrtt_wrapper/config/seggerrtt.cmake)
-include(${PROJECT_LIBRARY_DIR}/nrf5sdk_wrapper/config/nrf5sdk.cmake)
-include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
 include(${PROJECT_LIBRARY_DIR}/tflite-micro_wrapper/config/tflite_micro.cmake)
 
 ####
@@ -28,11 +25,7 @@ set(INCLUDE__APP TRUE)
 set(APP__NAME "tflite_hello_world")
 
 get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
-string(TOLOWER ${UBINOS__BSP__BOARD_MODEL} _temp_board_model)
-set(_temp_softdevice_name "blank")
 
-include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model}/${_temp_softdevice_name}/config)
-include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model})
 include_directories(${_tmp_source_dir})
 
 file(GLOB_RECURSE _tmp_sources
